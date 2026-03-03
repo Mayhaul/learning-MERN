4967e6aae0f04a451294c8383339a2bbabfd99ca
Mayhaul <mehulv2k6@gmail.com>
2026-03-03 22:25:41 +0530

    Event Bubbling (to work with new added elements, it gives it the DOM properties that we had assigned to some prev elements)

--- toDoList/app.js
+++ toDoList/app.js
@@ -1,25 +1,29 @@
 let inp = document.querySelector('input');
 let btn = document.querySelector('button');
 let ul = document.querySelector('ul');
-btn.addEventListener('click',function(){
+
+btn.addEventListener('click', function() {
     console.log(inp.value);
+    
     let NewTask = document.createElement('li');
+    // Adding Bootstrap classes to the LI
+    NewTask.classList.add('list-group-item', 'd-flex', 'justify-content-between', 'align-items-center');
     NewTask.innerText = inp.value;
+    
     let btnDlt = document.createElement('button');
-    btnDlt.classList.add('dlt');
+    // Adding Bootstrap classes to the Button
+    btnDlt.classList.add('dlt', 'btn', 'btn-danger', 'btn-sm');
     btnDlt.innerText = 'delete';
+    
     ul.appendChild(NewTask);
     NewTask.appendChild(btnDlt);
     inp.value = "";
 });
-let dlt = document.querySelectorAll('.dlt');
 
-for(i of dlt){
-    i.addEventListener('click',function(){
-    console.log("deleted");
+ul.addEventListener('click', function(event) {
+    if (event.target.nodeName == "BUTTON") {
+        let listItem = event.target.parentElement;
+        listItem.remove();
+        console.log('deleted');
+    }
 });
-
-}
-
-
-
