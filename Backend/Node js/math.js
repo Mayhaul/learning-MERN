3e482927d392dfe22a56b87f1ff1317deb248026
Mayhaul <mehulv2k6@gmail.com>
2026-03-13 10:14:11 +0530

    require, module.exports

---
+++ Backend/Node js/math.js
@@ -0,0 +1,9 @@
+const PI = 3.14;
+const g = 9.8;
+// module.exports returns an object by default.
+// but u can change that by specifying.
+
+module.exports = {
+    PI : PI,
+    g : g
+}
---
+++ Backend/Node js/nodeapp.js
@@ -0,0 +1,7 @@
+// process.argv returns the arguements passed while opening/running a file.
+// it returns 2 paths by default.
+
+console.log(process.argv)
+
+const val = require("./math");
+console.log(val.PI);
