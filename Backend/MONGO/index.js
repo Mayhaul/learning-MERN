6ea5b87b79c9768763696bccb2355a3fb0f532ce
Mayhaul <mehulv2k6@gmail.com>
2026-04-22 14:57:01 +0530

    mongosh mongodb CRUD operations. mongoose in js

--- Backend/MONGO/index.js
+++ Backend/MONGO/index.js
@@ -6,3 +6,28 @@ mongoose.connect("mongodb://127.0.0.1:27017/college")
     console.log("connected");
 })
 .catch((err) => console.log(err));
+
+const teacherSchema = new mongoose.Schema({
+    name: { type: String, required: true },
+    info: [
+        {
+            cluster: { type: String, required: true },
+            department: { type: String, required: true }
+        }
+    ]
+});
+
+const teacher = mongoose.model("teacher",teacherSchema);
+
+const t1 = new teacher({
+    name : "harjeet Singh",
+    info:[{
+        cluster: "alpha",
+        department: "AIML"
+    }]
+});
+
+t1.save()
+.then(()=>{
+    console.log("Added");
+});
